43bf467d126cb00de0afe23eb215970c21ad0a18
Sangram3698 <sngrmbhandari@gmail.com>
2024-11-29 22:37:12 +0545

    public and view files

--- index.js
+++ index.js
@@ -10,14 +10,41 @@ app.use(express.urlencoded({extended:true})) //for parsing the data
 
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
-app.set(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,"public")))//
+
+let posts=[
+  {username:"Sangram",
+    content:"Hello there"
+  },
+  {username:"Raju",
+    content:"Small step to understand REST"
+  },
+  {username:"Shyan",
+    content:"REST means representational state change"
+  },
+  {username:"Krishna",
+    content:"Get,post,put,patch,delete"
+  },
+  {username:"Hari",
+    content:"We will see all"
+  },
+];
 
 
 
 const port=process.env.PORT
 
-app.get("/",(req,res)=>{
-  res.status(200).send("Hello from rest!!")
+app.get("/posts",(req,res)=>{
+  res.render("index.ejs",{posts})
+})
+app.get("/posts/new",(req,res)=>{
+  res.render("new.ejs")
+})
+
+app.post("/posts",(req,res)=>{
+  let{username,content}=req.body
+  posts.push({username,content})
+  res.redirect("/posts")
 })
 
 app.listen(port,()=>{
