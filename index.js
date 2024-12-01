3ae4bfdf05cd18e4d9af4e2e7d0e6534e5ddf0fe
Sangram3698 <sngrmbhandari@gmail.com>
2024-12-01 12:25:19 +0545

    Complete crud using REST concept

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express=require("express")
 const dotenv=require("dotenv")
 const path=require("path")
 const {v4:uuidv4}=require("uuid")
+const methodOverride=require("method-override")
 
 const app=express()
 dotenv.config()
 
 
 app.use(express.urlencoded({extended:true})) //for parsing the data
+app.use(methodOverride("_method"))
 
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
@@ -71,12 +73,18 @@ app.patch("/posts/:id",(req,res)=>{
   let post=posts.find((p)=>id===p.id)
   post.content=newContent
   console.log(id)
-  res.send("Patch working")
+  res.redirect("/posts")
 })
 app.get("/posts/:id/edit",(req,res)=>{
   let {id}=req.params;
   let post=posts.find((p)=>id==p.id)
   res.render("edit.ejs",{post})
+})
+
+app.delete("/posts/:id",(req,res)=>{
+  let{id}=req.params;
+  posts=posts.filter((p)=>id!==p.id)
+  res.redirect("/posts")
 })
 
 app.listen(port,()=>{
